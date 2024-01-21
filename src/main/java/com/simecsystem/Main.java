0def593d9f0f8aa58de9a6ca157f40a5b5514dc4
Shamil55 <kayes.dds@gmail.com>
2024-01-21 12:45:18 +0600

    running jvm 11

--- src/main/java/com/simecsystem/Main.java
+++ src/main/java/com/simecsystem/Main.java
@@ -2,6 +2,7 @@ package com.simecsystem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for(;;)System.out.println("Hello world!");
     }
 }
