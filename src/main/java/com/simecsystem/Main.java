c4f95f9f4f20a43acdfbd3ae36fadccc959585e1
asraful009 <asraful009@gmail.com>
2024-01-21 14:41:34 +0600

    h

--- src/main/java/com/simecsystem/Main.java
+++ src/main/java/com/simecsystem/Main.java
@@ -1,8 +1,18 @@
 package com.simecsystem;
 
-public class Main {
-    public static void main(String[] args) {
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.Security;
 
-        for(;;)System.out.println("Hello world!");
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Provider provider = Security.getProvider("SunPKCS11");
+        String configName = "./pkcs11.cfg";
+        provider = provider.configure(configName);
+//        Security.addProvider(provider);
+//        provider.getService("KeyStore", "PKCS11");
     }
 }
