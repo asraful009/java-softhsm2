c21202358677fc4456d9aae00d308629bcc9395f
Shamil55 <kayes.dds@gmail.com>
2024-01-30 17:42:56 +0600

    sign done

--- py-softhsm/dsa.py
+++ py-softhsm/dsa.py
@@ -9,16 +9,13 @@ token = lib.get_token(token_label='dgdp')
 # token = lib.get_token(token_label='29thJanNewToken')
 data = b'hi all'
 
+# Open a session on our token
+# with token.open(user_pin='Pwforsafenet5110!') as session:
+# with token.open(user_pin='1234') as session:
 with token.open(user_pin='aSdf1234**') as session:
+#     pub, priv = session.generate_keypair(pkcs11.KeyType.DSA,2048)
+    pub, priv = session.generate_keypair(pkcs11.KeyType.RSA, 2048)
 
-
-    objects = session.findObjects([
-        (Attribute.CLASS, CKO_CERTIFICATE)
-    ])
-
-    for obj in objects:
-        cert_value = session.getAttributeValue(obj, [CKA_VALUE])[0].to_bytes()
-
-        # Display or process the certificate as needed
-        print("Certificate:", cert_value.hex())
-
+    # Sign
+    signature = priv.sign(data)
+    print(signature)
--- src/main/java/com/simecsystem/GetCertificateDongle.java
+++ src/main/java/com/simecsystem/GetCertificateDongle.java
@@ -2,14 +2,33 @@ package com.simecsystem;
 
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import java.io.IOException;
+import javax.xml.crypto.dsig.Reference;
+import javax.xml.crypto.dsig.SignedInfo;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMSignContext;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.Enumeration;
 
 
@@ -20,6 +39,7 @@ public class GetCertificateDongle {
             PrivateKey privateKey = null;
             char[] password = "aSdf1234**".toCharArray();
             String configName = "/Users/imac/projects/dgdp/java-softhsm2/pkcs11.cfg";
+            String xmlFilePath = "/Users/imac/projects/dgdp/java-softhsm2/test.xml";
             Provider p = Security.getProvider("SunPKCS11");
             p = p.configure(configName);
             Security.addProvider(p);
@@ -32,21 +52,71 @@ public class GetCertificateDongle {
                 alias = e.nextElement();
                 String aliasTemp = URLEncoder.encode(alias, StandardCharsets.UTF_8);
                 X509Certificate certificate = (X509Certificate) keyStore.getCertificate(alias);
+//                System.out.println(certificate);
+                PrivateKey key = (PrivateKey)keyStore.getKey(alias, "aSdf1234**".toCharArray());
+                System.out.println("Key: " + key);
                 PublicKey publicKey = certificate.getPublicKey();
                 byte[] encodedPublicKey = publicKey.getEncoded();
                 String b64PublicKey = Base64.getEncoder().encodeToString(encodedPublicKey);
-                System.out.println("Name: " + certificate.getSubjectX500Principal().getName());
-                System.out.println("Name: " + b64PublicKey);
-                System.out.println("Name: " + aliasTemp);
-                System.out.println(alias.replaceAll("\\s", ""));
 
                 System.out.println("Certificate: " + alias);
+                Security.addProvider((Provider)new BouncyCastleProvider());
+                Provider pro = Security.getProvider("BC");
+                System.out.println(pro.getName());
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                dbf.setNamespaceAware(true);
+                Document doc = dbf.newDocumentBuilder().parse(new FileInputStream(xmlFilePath));
+                System.out.println(doc.getDocumentElement());
+                XMLSignatureFactory signatureFactory = XMLSignatureFactory.getInstance("DOM");
+                System.out.println(key);
+                DOMSignContext dsc = new DOMSignContext(key, doc.getDocumentElement());
+                Reference ref =
+                    signatureFactory.newReference("",
+                        signatureFactory.newDigestMethod("http://www.w3.org/2001/04/xmlenc#sha256", null),
+                        Collections.singletonList(
+                                signatureFactory.newTransform("http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+                                (TransformParameterSpec)null)), null, null);
+                SignedInfo si = signatureFactory.newSignedInfo(signatureFactory
+                                .newCanonicalizationMethod("http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments",
+                                        (C14NMethodParameterSpec)null),
+                        signatureFactory.newSignatureMethod("http://www.w3.org/2000/09/xmldsig#rsa-sha1", null),
+                        Collections.singletonList(ref));
+                KeyInfoFactory kif = signatureFactory.getKeyInfoFactory();
+                KeyValue kv = kif.newKeyValue(certificate.getPublicKey());
+                KeyInfo ki = kif.newKeyInfo(Collections.singletonList(kv));
+                XMLSignature signature = signatureFactory.newXMLSignature(si, ki);
+                signature.sign(dsc);
+                System.out.println(doc);
+                Node root = doc.getDocumentElement();
+
+                // Print information about the root element
+                System.out.println("Root Element: " + root.getNodeName());
+
+                // Process child nodes (elements) of the root
+                NodeList childNodes = root.getChildNodes();
+                for (int i = 0; i < childNodes.getLength(); i++) {
+                    Node childNode = childNodes.item(i);
+
+                    // Check if the node is an element (as opposed to text, comment, etc.)
+                    if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                        // Print information about each child element
+                        System.out.println("Element Name: " + childNode.getNodeName());
+                        System.out.println("Element Value: " + childNode.getTextContent());
+                    }
+                }
+                saveXmlDocument(doc, xmlFilePath);
             }
         } catch (Exception e) {
             System.out.println(e);
 
         }
         }
+    private static void saveXmlDocument(Document doc, String filePath) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(filePath)));
+        System.out.println("Signed XML document saved to: " + filePath);
+    }
 
 
     }
